743bfa85c3f1e74c2bfcfc7468e73ab2e3bc1eb1
Md. Nadim Mahmud Borad <nadim.ict20@gmail.com>
2024-10-05 22:02:29 +0600

    Update Typing Speed and Accuracy Calculator.cpp

--- Typing Speed and Accuracy Calculator.cpp
+++ Typing Speed and Accuracy Calculator.cpp
@@ -1,13 +1,12 @@
 // nadim [ict,mbstu]
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
-   system("color 29");
+   system("color 1");
 vector<vector<string>> showStr(5);
     showStr[0]={"power", "time", "able", "congratulation", "zombie", "enough"};
-    showStr[1]={"done", "from", "nation", "document", "include", "tag"};
-    showStr[2]={"jumps","a","cute","quick","brown","cat","fox","come","over","the","time","zoo","lazy","sleep","dog","done", "from", "nation", "document", "include" };
+    showStr[1]={"jumps","a","cute","quick","brown","cat","fox","come","over","the","time","zoo","lazy","sleep","dog","done", "from", "nation", "document", "include" };
+    showStr[2]={"done", "from", "nation", "document", "include", "tag"};
     showStr[3]={"compare", "science", "desert", "past", "principle", "there", "too", "weather", "between", "again", "except", "example", "successive", "immoral", "industrial"};
     showStr[4]={"gmail", "student", "have", "time", "information", "relay", "there", "because", "weather", "without", "again", "except", "example", "successive", "immoral", "industrial"};
 
@@ -16,7 +15,9 @@ vector<pair<double,pair<double,double>>> history;
     history.push_back(make_pair(15,make_pair(46,99)));
 
     main_menu:
-
+    cout<<"\n";
+    cout<<"\n";
+    cout<<"\t\tMain menu"<<"\n";
     cout<<"\n";
     cout<<"\tTest your typing speed\n";
     cout<<"\n";
@@ -25,7 +26,7 @@ vector<pair<double,pair<double,double>>> history;
     cout<<"\t[3] Exit\n";
 
    int choice;
-   cout<<"\n\tSelect any by number: ";
+   cout<<"\n\tSelect any by option: ";
    cin>>choice;
 
 
@@ -33,8 +34,10 @@ vector<pair<double,pair<double,double>>> history;
        {
 
          system("CLS");
+
+             int oneOfFive=rand()%5;
             cout<<"=>Press Enter to start timer then start typing the text below.\n\n\n";
-            for(auto it=showStr[2].begin();it!=showStr[2].end();it++)
+            for(auto it=showStr[oneOfFive].begin();it!=showStr[oneOfFive].end();it++)
             cout<<*it<<' ';
             cout<<"\n";
 
@@ -57,11 +60,11 @@ vector<pair<double,pair<double,double>>> history;
             int in_n=input.size();
                for(int j=0;j<in_n;j++)
                      {
-            if(find(showStr[2].begin(),showStr[2].end(),input[j])!=showStr[2].end())
+            if(find(showStr[oneOfFive].begin(),showStr[oneOfFive].end(),input[j])!=showStr[oneOfFive].end())
                   rightInput++;
                   }
 
-    double wordCount=min(showStr[2].size(),input.size());   // calculating
+    double wordCount=min(showStr[oneOfFive].size(),input.size());   // calculating
     double sec=(1.0*(en-st)/CLOCKS_PER_SEC);
     double min=((1.0*(en-st)/CLOCKS_PER_SEC)/60);
     double speed=wordCount/min;
@@ -109,10 +112,11 @@ vector<pair<double,pair<double,double>>> history;
      if(choice==2)
      {
             system("CLS");
-         cout<<"\nWord typed\tTyping speed\tAccuracy"<<'\n';
+            cout<<"\n\t\t\tHistory"<<"\n";
+         cout<<"\n\tWord typed\tTyping speed\tAccuracy"<<'\n';
          for(int i=0;i<history.size();i++)
          {
-            cout<<"    "<<history[i].first<<"\t\t"<<"    "<<history[i].second.first<<" WPM\t "<<history[i].second.second<<" %\n";
+            cout<<"\t    "<<history[i].first<<"\t\t"<<"   "<<history[i].second.first<<" WPM\t  "<<history[i].second.second<<" %\n";
          }
          cout<<"\n\n";
 
@@ -137,4 +141,3 @@ vector<pair<double,pair<double,double>>> history;
 
    return 0;
 }
-
